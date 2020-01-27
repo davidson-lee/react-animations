02cb83d6e2bf43e930822c31082c8c722c9dcede
Davidson Lee <contact@davidsonlee.me>
2020-01-27 17:18:12 -0500

    Animated menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu'
+
+import { motion } from 'framer-motion'
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className="App">
+      <div class="menu-toggle" onClick={() => { setIsOpen(!isOpen) }} >
+        { isOpen ? "X" : "O" }
+      </div>
+      <Menu isOpen={isOpen} />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <motion.img animate={{ opacity: 1 }} initial={{ opacity: 0 }} src={logo} className="App-logo" alt="logo" />
+        <motion.p animate={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: "10vh" }} >
           Animations Test!
-        </p>
+        </motion.p>
       </header>
     </div>
   );
---
+++ src/components/Menu/index.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+
+import './styles.css'
+
+const Menu = ({ isOpen }) => {
+
+  const menu = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: 'easeOut'
+      }
+    },
+    closed: {
+      opacity: 0,
+      y: "-100%",
+      transition: {
+        ease: 'easeOut'
+      }
+    }
+  }
+
+  const items = {
+    open: {
+      transition: {
+        staggerChildren: 0.1
+      }
+    },
+    closed: {
+
+    }
+  }
+
+  const item = {
+    open: {
+      opacity: 1,
+      y: 0
+    },
+    closed: {
+      opacity: 0,
+      y: '-20%'
+    }
+  }
+
+  return (
+    <motion.div
+      animate={isOpen ? "open" : "closed"}
+      initial="closed"
+      variants={menu}
+      className="nav"
+    >
+      <motion.div variants={items} className="menu-item-container">
+        <motion.h3 variants={item}>Menu Item 1</motion.h3>
+        <motion.h3 variants={item}>Menu Item 2</motion.h3>
+        <motion.h3 variants={item}>Menu Item 3</motion.h3>
+        <motion.h3 variants={item}>Menu Item 4</motion.h3>
+        <motion.h3 variants={item}>Menu Item 5</motion.h3>
+      </motion.div>
+    </motion.div>
+  )
+}
+
+export default Menu
