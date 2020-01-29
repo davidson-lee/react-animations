69e20b3359c02e542f30c4a8742205ee05b0da4a
Davidson Lee <contact@davidsonlee.me>
2020-01-29 17:34:33 -0500

    Transformed colors to Images, added cycling and prev/next

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/Menu'
@@ -8,17 +8,34 @@ import { motion } from 'framer-motion'
 function App() {
 
   const [isOpen, setIsOpen] = useState(false)
-  const [bg, setBg] = useState("green")
-  const colorList = ["red", "blue", "green"]
+  const [bg, setBg] = useState("coffee")
+  const [isPlaying, setIsPlaying] = useState(true)
+  const colorList = ["coffee", "cake", "strawberry"]
+
+  const prevImg = useRef();
+
+  useEffect(() => {
+    if (isPlaying) {
+
+      const timeout = setTimeout(() => {
+
+        handleNext()
+      }, 3000)
+
+      return () => {
+        clearTimeout(timeout)
+      }
+    }
+  })
 
   const getTextColor = (color) => {
     switch(color) {
-      case "blue":
-        return "rgb(0,0,255)"
-      case "red":
-        return "rgb(255,0,0)"
-      case "green":
-        return "rgb(0,120,0)"
+      case "coffee":
+        return "rgb(240,240,190)"
+      case "strawberry":
+        return "rgb(255,230,230)"
+      case "cake":
+        return "rgb(0,255,0)"
       case "white":
         return "rgb(255,255,255)"
       default:
@@ -28,45 +45,74 @@ function App() {
 
   const getBgColor = (color) => {
     switch(color) {
-      case "blue":
-        return "#282c34"
-      case "red":
-        return "rgb(100,50,50)"
-      case "green":
-        return "rgb(50,100,50)"
+      case "coffee":
+        return "https://www.shambhalaschool.org/wp-content/uploads/2017/11/pp-hot-coffee-rf-istock.jpg"
+      case "cake":
+        return "https://foodmeanderings.com/wp-content/uploads/2019/10/Chocolate-Mayonnaise-Cake-Recipe.jpg"
+      case "strawberry":
+        return "https://hips.hearstapps.com/clv.h-cdn.co/assets/15/22/1600x1066/gallery-1432670584-strawberry-facts8b.jpg"
       default:
-        return "#282c34"
+        return "https://www.shambhalaschool.org/wp-content/uploads/2017/11/pp-hot-coffee-rf-istock.jpg"
     }
   }
 
-  const getVariants = () => {
-    const variants = {
-      active: {
-        background: "rgb(255,255,255)",
-        color: getTextColor(bg),
-        scale: 1.1,
-        transition: {
-          duration: 1
-        }
+  const handlePrev = () => {
+    const currentIndex = colorList.indexOf(bg)
+    const lastIndex = colorList.length - 1
+
+
+
+    if (currentIndex === 0) {
+      setBg(colorList[lastIndex])
+    } else {
+      setBg(colorList[currentIndex - 1])
+    }
+  }
+
+  const handleNext = () => {
+    const currentIndex = colorList.indexOf(bg)
+    const lastIndex = colorList.length - 1
+
+    if (currentIndex === lastIndex) {
+      setBg(colorList[0])
+    } else {
+      setBg(colorList[currentIndex + 1])
+    }
+  }
+
+  const variants = {
+    active: {
+      opacity: 1,
+      transition: {
+        duration: 1
+      }
+    },
+    inactive: {
+      opacity: 0,
+      transition: {
+        duration: 1
       }
     }
+  }
 
-    colorList.forEach(color => {
-      variants[color] = {
-        background: getBgColor(color),
-        scale: 1,
-        color: getTextColor("white"),
-        transition: {
-          duration: 1
-        }
+  const progressVariants = {
+    full: {
+      width: '100%',
+      transition: {
+        duration: 3,
+        ease: 'linear'
       }
-    })
-
-    return variants
+    },
+    empty: {
+      width: '0%',
+      transition: {
+        ease: 'easeOut'
+      }
+    }
   }
 
   const isActive = (color) => {
-    return bg === color ? "active" : color
+    return bg === color ? "active" : "inactive"
   }
 
   const capitalize = (string) => {
@@ -81,7 +127,7 @@ function App() {
 
       <Menu isOpen={isOpen} />
       <motion.header
-        variants={getVariants()}
+        variants={variants}
         animate={bg}
         initial={"blue"}
         className="App-header"
@@ -90,19 +136,36 @@ function App() {
         {
           colorList.map(color => {
             return (
-              <motion.p
-                key={color}
-                variants={getVariants()}
-                animate={isActive(color)}
-                onClick={() => { setBg(color) }}
-                className="btn"
-              >
-                {capitalize(color)}
-              </motion.p>
+              <React.Fragment>
+                <motion.img
+                  src={getBgColor(color)}
+                  key={color}
+                  variants={variants}
+                  animate={isActive(color)}
+                  className="bg-img"
+                >
+                </motion.img>
+                <div className="progress-container">
+                  <motion.div
+                    variants={progressVariants}
+                    animate={(bg === color && isPlaying) ? "full" : "empty"}
+                    initial={"empty"}
+                    className="progress"
+                  />
+                </div>
+              </React.Fragment>
             )
           })
         }
 
+
+        <div>
+          <h2 onClick={handlePrev}>Prev</h2>
+          <h2 onClick={handleNext}>Next</h2>
+          <h2 onClick={() => { setIsPlaying(!isPlaying)}}>
+            { isPlaying ? "PAUSE" : "PLAY" }
+          </h2>
+        </div>
       </motion.header>
     </div>
   );
